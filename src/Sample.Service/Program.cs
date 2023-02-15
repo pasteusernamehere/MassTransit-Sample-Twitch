04d96cde1627ceeb2a3c1cfab979bf3fc3418666
Scott Morton <mortonsp@gmail.com>
2023-02-15 16:03:03 -0500

    Converting Sample.Service to use top level statements

--- src/Sample.Service/Program.cs
+++ src/Sample.Service/Program.cs
@@ -1,63 +1,54 @@
-﻿namespace Sample.Service;
-
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
-using Components.Consumers;
 using MassTransit;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Sample.Components.Consumers;
 
-internal class Program
-{
-    private static async Task Main(string[] args)
+var isService = !(Debugger.IsAttached || args.Contains("--console"));
+
+var builder = new HostBuilder().ConfigureAppConfiguration((hostingContext, config) =>
     {
-        var isService = !(Debugger.IsAttached || args.Contains("--console"));
+        config.AddJsonFile("appsettings.json", true);
+        config.AddEnvironmentVariables();
 
-        var builder = new HostBuilder().ConfigureAppConfiguration((hostingContext, config) =>
+        if (args != null)
+        {
+            config.AddCommandLine(args);
+        }
+    })
+    .ConfigureServices((hostingContext, services) =>
+    {
+        services.TryAddSingleton(KebabCaseEndpointNameFormatter.Instance);
+        services.AddMassTransit(x =>
+        {
+            x.AddConsumersFromNamespaceContaining<SubmitOrderConsumer>();
+            x.UsingRabbitMq((context, cfg) =>
             {
-                config.AddJsonFile("appsettings.json", true);
-                config.AddEnvironmentVariables();
-
-                if (args != null)
+                cfg.Host("localhost", "/", h =>
                 {
-                    config.AddCommandLine(args);
-                }
-            })
-            .ConfigureServices((hostingContext, services) =>
-            {
-                services.TryAddSingleton(KebabCaseEndpointNameFormatter.Instance);
-                services.AddMassTransit(x =>
-                {
-                    x.AddConsumersFromNamespaceContaining<SubmitOrderConsumer>();
-                    x.UsingRabbitMq((context, cfg) =>
-                    {
-                        cfg.Host("localhost", "/", h =>
-                        {
-                            h.Username("guest");
-                            h.Password("guest");
-                        });
-                        cfg.ConfigureEndpoints(context);
-                    });
+                    h.Username("guest");
+                    h.Password("guest");
                 });
-                services.AddHostedService<MassTransitHostedService>();
-            })
-            .ConfigureLogging((hostingContext, logging) =>
-            {
-                logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
-                logging.AddConsole();
+                cfg.ConfigureEndpoints(context);
             });
+        });
+        services.AddHostedService<MassTransitHostedService>();
+    })
+    .ConfigureLogging((hostingContext, logging) =>
+    {
+        logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+        logging.AddConsole();
+    });
 
-        if (isService)
-        {
-            await builder.UseWindowsService().Build().RunAsync();
-        }
-        else
-        {
-            await builder.RunConsoleAsync();
-        }
-    }
+if (isService)
+{
+    await builder.UseWindowsService().Build().RunAsync();
+}
+else
+{
+    await builder.RunConsoleAsync();
 }
